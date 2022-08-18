e8888d61bf6ea6ce27c8f6dbc8c232ce3ca279ca
Maureenkamau03 <mkaymaureen@gmail.com>
2022-08-19 00:48:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  let hq = 42;
+  return Math.abs(hq - location);
+}
+// distanceFromHqInBlocks(43);
+function distanceFromHqInFeet(location) {
+  let hq = 42;
+  return Math.abs(hq - location) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+// function calculatesFarePrice(start, destination) {
+//   let distance = Math.abs(destination - start) * 264;
+//   if (distance > 400) {
+//     return `cannot travel that far`;
+//   } else if (distance > 2000) {
+//     return `25 dollars`;
+//   } else if (distance > 400) {
+//     return `2 cents per foot`;
+//   } else {
+//     return `cannot travel that far`;
+//   }
+// }
+function calculatesFarePrice(start, destination) {
+  let distance = Math.abs(destination - start) * 264;
+  if (distance > 2500) {
+    return `cannot travel that far`;
+  } else if (distance > 2000) {
+    return 25;
+  } else if (distance > 400) {
+    return 2.56;
+  } else {
+    return 0;
+  }
+}
